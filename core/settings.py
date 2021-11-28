d4c6c5aa79c9b0022edc7470bfd57a5b736ca198
Mulyawan Sentosa <development@flazhost.com>
2021-11-29 01:03:22 +0700

    Update

--- core/settings.py
+++ core/settings.py
@@ -21,7 +21,8 @@ DEBUG = config('DEBUG', default=True, cast=bool)
 
 # load production server from .env
 # ALLOWED_HOSTS = ['localhost', '127.0.0.1', config('SERVER', default='127.0.0.1')]
-ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['sifoda.alice-abm.id', 'www.sifoda.alice-abm.id']
 
 # Application definition
 
